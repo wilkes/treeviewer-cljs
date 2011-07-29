59d167d23095aae56c897de12963af316e95873e
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-07-29 07:38:14 -0500

    addes support for arrays and objects (1-level deep)

--- src/treeviewer/core.cljs
+++ src/treeviewer/core.cljs
@@ -1,10 +1,18 @@
 (ns treeviewer.core
   (:require [goog.dom :as dom]
-            [goog.ui.tree.TreeControl :as TreeControl]))
+            [goog.ui.tree.TreeControl :as TreeControl]
+            [goog.string :as s]
+            [goog.object :as o]))
+
+(declare add-data)
 
 (def data {:a 1 :b [1 2] :c {:d 1 :e 2 :f "foo"}})
 
-(declare add-data)
+(defn array? [x]
+  (= "array" (goog.typeOf x)))
+
+(defn object? [x]
+  (= "object" (goog.typeOf x)))
 
 (defn tree-control []
   (goog.ui.tree.TreeControl.
@@ -19,11 +27,14 @@
     (.add node child)
     child))
 
+(defn set-html [node s]
+  (.setHtml node (s/htmlEscape s)))
+
 (defn label [node x]
-  (.setHtml node (pr-str x)))
+  (set-html node (pr-str x)))
 
 (defn short-label [node x]
-  (.setHtml node (str (.substring (pr-str x) 0 10) "...")))
+  (set-html node (str (.substring (pr-str x) 0 10) "...")))
 
 (defn add-map [node m]
   (label node m)
@@ -32,19 +43,42 @@
       (label child [k v])
       (add-data (create-child child) v))))
 
+(defn add-object [node obj]
+  (label node obj)
+  (o/forEach obj
+             (fn [v k obj]
+               (let [child (create-child node)]
+                 (label child k)
+                 (label (create-child child) v)))))
+
 (defn add-seq [node l]
   (label node l)
   (doseq [item l]
     (add-data (create-child node) item)))
 
 (defn add-data [node x]
-  (let [add-to (cond (map? x) add-map
-                     (sequential? x) add-seq
-                     :otherwise label)]
+  (let [add-to
+        (cond (map? x) add-map
+              (or (sequential? x)
+                  (set? x)
+                  (array? x)) add-seq
+              (object? x) add-object
+              :otherwise label)]
     (add-to node x)
     node))
 
-(-> (tree-control)
-    (add-data data)
-    (render "display"))
+(defn render-sample []
+  #_(dom/setTextContent (dom/getElement "debug") (pr-str (conj #{:a} :b)))
+  (-> (tree-control)
+      (add-data data)
+      (render "display"))
+  (-> (tree-control)
+      (add-data [1 2
+                 {:a 1}
+                 [:b :c :d]
+                 {:b '(1 2 3)} #{5 3 2} (array 1 2 3)
+                 (tree-control)]
+                )
+      (render "display")))
 
+(render-sample)
