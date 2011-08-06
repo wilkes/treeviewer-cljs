7490dfd8780343193b250bd91e54f78f56b59699
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-08-06 08:28:35 -0500

    read data structure from textarea

--- src/treeviewer/core.cljs
+++ src/treeviewer/core.cljs
@@ -6,17 +6,17 @@
             [goog.debug.DivConsole :as DivConsole]
             [goog.debug.LogManager :as LogManager]
             [goog.events :as events]
-            [goog.events.EventType :as event-type]
-            [goog.ui.tree.BaseNode.EventType :as EventType]))
+            [goog.ui.tree.BaseNode.EventType :as EventType]
+            [cljs.reader :as reader]))
 
 (def debug-console
-  (doto (goog.debug.DivConsole. (dom/getElement "div-console"))
+  (doto (goog.debug.DivConsole. (dom/$ "div-console"))
     (.setCapturing true)))
 
 (def logger (goog.debug.LogManager/getRoot))
 
-(defn info [s]
-  (.info logger s))
+(defn info [& s]
+  (.info logger (apply str s)))
 
 (declare add-data)
 
@@ -30,13 +30,21 @@
   (goog.ui.tree.TreeControl. "root"
                              (.defaultConfig goog.ui.tree.TreeControl)))
 
+(defn render [component element-id]
+  (.render component (dom/$ element-id))
+  component)
+
+(defn clear-tree [element-id]
+  (dom/removeChildren (dom/$ element-id)))
+
 (defn render-tree [element-id data]
-  (-> (tree-control)
-      (add-data data)
-      (.render (dom/getElement element-id))))
+  (let [tree (tree-control)]
+    (-> tree
+        (add-data data)
+        (render element-id))))
 
 (defn create-child [node]
-  (let [child (.createNode (.getTree node ()) "")]
+  (let [child (.createNode (. node (getTree)) "")]
     (.add node child)
     child))
 
@@ -61,42 +69,47 @@
   (let [child (create-child node)]
     (events/listen node
                    goog.ui.tree.BaseNode.EventType/BEFORE_EXPAND
-                   #(do (info "Exanding")
-                        (o/forEach obj
-                                   (fn [v k obj]
-                                     (label child k)
-                                     (add-data (create-child child) v)))
-                        (info "Expanded")))))
+                   #(o/forEach obj
+                               (fn [v k obj]
+                                 (label child k)
+                                 (add-data (create-child child) v))))))
 
 (defn add-seq [node l]
   (label node l)
   (doseq [item l]
     (add-data (create-child node) item)))
 
+(defn add-seq? [x]
+  (or (sequential? x)
+      (set? x)
+      (array? x)))
+
 (defn add-data [node x]
+  (info "adding " x)
   (let [add-to (cond (map? x) add-map
-                     (or (sequential? x)
-                         (set? x)
-                         (array? x)) add-seq
+                     (add-seq? x) add-seq
                      (object? x) add-object
                      :otherwise label)]
     (add-to node x)
     node))
 
-(defn render-sample []
-  (info "Start...")
-  #_(dom/setTextContent (dom/getElement "debug") (pr-str (conj #{:a} :b)))
-  (render-tree "display" {:a 1
-                          :b [1 2]
-                          :c {:d 1
-                              :e 2
-                              :f "foo"}})
-  (render-tree "display"
-               [1 2
-                {:a 1}
-                [:b :c :d]
-                {:b '(1 2 3)} #{5 3 2} (array 1 2 3)
-                (tree-control)])
-  (info "Done."))
+(defn do-button-clicked []
+  (clear-tree "display")
+  (let [v (.value (dom/$ "text-input"))
+        r (reader/read-string v)] ;; needs error handling
+    (info v)
+    (render-tree "display" r)))
 
-(render-sample)
+
+(defn render-sample []
+  (render-tree "display"
+               {:a 1
+                :b [1 2 3]
+                :c #{4 5 6}}))
+
+(defn main []
+  (events/listen (dom/$ "treerize")
+                 "click"
+                 do-button-clicked))
+
+(main)
